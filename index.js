a9de3b8fdff94858c45bd433d326ed68c9c7839d
KaKarlinn <carlosleo2525@gmail.com>
2024-01-18 19:19:40 -0300

    18/01

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 let button = document.getElementById('button')
+let conteudo = document.getElementById('conteudo')
+let faixa = document.getElementById('faixa')
 
 button.addEventListener('click', () => {
+ 
+    sortearHeroi()
+})
+
+
+
+
+const sortearHeroi = () => {
+    
     const geradorDeNumeroAleatorio = Math.round(Math.random() * 60)
     console.log('a',geradorDeNumeroAleatorio)
 
+        fetch(`https://akabab.github.io/superhero-api/api/id/${geradorDeNumeroAleatorio}.json`).then((response) => response.json())
+        .then((data) => {
+            const {name : nomeDosHerois,images} = data
+            console.log(nomeDosHerois,images.md)
+            faixa.innerText = nomeDosHerois
 
+            let imagemNova = document.createElement("img")
+            imagemNova.id = 'imagem'
+            imagemNova.src = `${images.md}`
 
-    fetch(`https://akabab.github.io/superhero-api/api/id/${geradorDeNumeroAleatorio}.json`).then((response) => response.json())
-    .then((data) => console.log(data))
-    console.log('aiai que mina perversa')
-})
+            document.body.appendChild(imagemNova)
+            
+        }).catch((error) => {
+            (error.message)
+            
+        })
+    }
